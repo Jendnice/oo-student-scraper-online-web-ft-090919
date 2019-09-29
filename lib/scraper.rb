3e12634a15622ad3f97d24c88d363e3de88c7775
Jennifer Nicely <pipeline38@hotmail.com>
2019-09-29 14:40:34 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,14 +1,14 @@
-require 'nokogiri'
+# require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
-require_relative './student.rb'
+# require_relative './student.rb'
 
 class Scraper
 
-  def self.scrape_index_page(index_url)
-    doc = Nokogiri::HTML(open(https://learn-co-curriculum.github.io/student-scraper-test-page/index.html))
-    doc.css("")
+  #def self.scrape_index_page(index_url)
+   # doc = Nokogiri::HTML(open(https://learn-co-curriculum.github#.io/student-scraper-test-page/index.html))
+ #   doc.css("")
     
  # return [{:name =>, :location =>, :profile_url =>}, {}, etc.]
   end
