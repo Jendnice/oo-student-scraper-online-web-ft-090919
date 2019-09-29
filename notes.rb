b2edc67af2d59565815eb3f2ad20d5b00941aa53
Jennifer Nicely <pipeline38@hotmail.com>
2019-09-29 15:03:57 +0000

    Automatically backed up by Learn

--- notes.rb
+++ notes.rb
@@ -40,3 +40,21 @@ end
 # title: doc.css(".post").first.css("h2").text
 # schedule: doc.css(".post").first.css(".date").text
 # description: doc.css(".post").first.css("p").text
+
+__________________________________________________________
+
+def self.scrape_index_page(index_url)
+    page = Nokogiri::HTML(open(index_url))
+    students = []
+
+    page.css("div.student-card").each do |student|
+      name = student.css(".student-name").text
+      location = student.css(".student-location").text
+      profile_url = student.css("a").attribute("href").value
+      student_info = {:name => name,
+                :location => location,
+                :profile_url => profile_url}
+      students << student_info
+      end
+    students
+   end
