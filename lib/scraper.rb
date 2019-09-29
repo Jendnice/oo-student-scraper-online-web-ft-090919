176023dbea0615ea70d9eb293187776be345289a
Jennifer Nicely <pipeline38@hotmail.com>
2019-09-29 16:40:31 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -22,27 +22,27 @@ class Scraper
     students
  end
 
-#   def self.scrape_profile_page(profile_url)
-#     page = Nokogiri::HTML(open(profile_url))
-#     student = {}
+  def self.scrape_profile_page(profile_url)
+    page = Nokogiri::HTML(open(profile_url))
+    student = {}
   
-#   social = page.css("div.vitals-container div.social-icon-container a").collect do |icon| icon.attribute("href").value end 
+  social = page.css("div.vitals-container div.social-icon-container a").collect do |icon| icon.attribute("href").value end 
   
-#   social.each do |link| 
-#     if link.include?("twitter")
-#       student[:twitter] = link
-#     elsif link.include?("linkedin")
-#       student[:linkedin] = link 
-#     elsif link.include?("github")
-#       student[:github] = link 
-#     elsif link.include?("blog")
-#       student[:blog] = link 
-#     end 
-#   end 
-#   student[:profile_quote] = page.css(".profile-quote").text
-#   student[:bio] = page.css("div.description-holder p").text
-#   student 
-# end 
+  social.each do |link| 
+    if link.include?("twitter")
+      student[:twitter] = link
+    elsif link.include?("linkedin")
+      student[:linkedin] = link 
+    elsif link.include?("github")
+      student[:github] = link 
+    elsif link.include?("blog")
+      student[:blog] = link 
+    end 
+  end 
+  student[:profile_quote] = page.css(".profile-quote").text
+  student[:bio] = page.css("div.description-holder p").text
+  student 
+end 
  
 # end 
   
